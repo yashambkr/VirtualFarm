f882708991b70001e04a67919d2f1ddd7dcdeb8f
Swaraj Mahindre <swarajmahindre5@gmail.com>
2021-12-23 17:42:46 +0530

    Updated The Login Page UI to look better using Material UI Components

--- app/src/main/java/com/example/virtualfarm2020/GraphActivity.java
+++ app/src/main/java/com/example/virtualfarm2020/GraphActivity.java
@@ -3,9 +3,30 @@ package com.example.virtualfarm2020;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class GraphActivity extends AppCompatActivity {
+    LineChart lineChart;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference myref;
+    LineDataSet lineDataSet= new LineDataSet(null, null);
+    ArrayList<ILineDataSet> iLineDataSets= new ArrayList<>();
+    LineData lineData;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +36,50 @@ public class GraphActivity extends AppCompatActivity {
         //when change button clicked
         Button btn = findViewById(R.id.pHButton);
         btn.setOnClickListener(v -> pHChange());
+
+        //line chart
+        lineChart=findViewById(R.id.pHLineChart);
+        firebaseDatabase= FirebaseDatabase.getInstance();
+        myref=firebaseDatabase.getReference().child("Sensor");
+        readData();
+
+    }
+
+    private void readData() {
+        myref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ArrayList<Entry> dataVal= new ArrayList<>();
+                if(snapshot.hasChildren()){
+//                    DataPoint dp= snapshot.child("pH").getValue(DataPoint.class);
+//                    dataVal.add(new Entry(Objects.requireNonNull(dp).getTs(), dp.getpH()));
+                    float ph= Float.parseFloat(String.valueOf(snapshot.child("pH").getValue()));
+                    //String currentTime = new SimpleDateFormat("HH:mm a", Locale.getDefault()).format(new Date());
+                    float ts= System.currentTimeMillis()/(1000*360);
+                    dataVal.add(new Entry(ts,ph));
+                    showChart(dataVal);
+                }else{
+                    lineChart.clear();
+                    lineChart.invalidate();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void showChart(ArrayList<Entry> dataVal) {
+        lineDataSet.setValues(dataVal);
+        lineDataSet.setLabel("pH");
+       // iLineDataSets.clear();
+        iLineDataSets.add(lineDataSet);
+        lineData= new LineData(iLineDataSets);
+        lineChart.clear();
+        lineChart.setData(lineData);
+        lineChart.invalidate();
     }
 
     //pHChange sets the new pH
