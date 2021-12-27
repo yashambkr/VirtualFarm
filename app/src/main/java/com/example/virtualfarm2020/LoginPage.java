8c633c465c472ac1b9513c69345bbb796f4efe25
roshniLingam <lingam_roshni.ghrcecs@raisoni.net>
2021-12-27 14:28:21 +0530

    Made changes in MainDashboard.java

--- app/src/main/java/com/example/virtualfarm2020/LoginPage.java
+++ app/src/main/java/com/example/virtualfarm2020/LoginPage.java
@@ -5,7 +5,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
