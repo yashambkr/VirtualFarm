89a876aa509a65034ae4eb2229aa32909e3eae79
Swaraj Mahindre <swarajmahindre5@gmail.com>
2021-12-31 14:22:22 +0530

    Redesigned The Application

--- app/src/main/java/com/example/virtualfarm2020/MainDashboard.java
+++ app/src/main/java/com/example/virtualfarm2020/MainDashboard.java
@@ -3,8 +3,11 @@ package com.example.virtualfarm2020;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 public class MainDashboard extends AppCompatActivity {
 
@@ -14,12 +17,24 @@ public class MainDashboard extends AppCompatActivity {
         setContentView(R.layout.activity_main_dashboard);
 
 
+        //Welcome Notification
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "virtual_farm")
+
+                .setContentTitle("Welcome")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
     }
+
+   /*
 
     public void openScreen(View view){
         Intent i = new Intent(this, GraphActivity.class);
         startActivity(i);
     }
+*/
+    
 
 
 
